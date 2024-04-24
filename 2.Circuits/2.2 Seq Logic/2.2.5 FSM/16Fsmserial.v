dc2bdaebfdae6a40166dc1b71826d58c4ff9d3df
jiawey1227 <yujiawei123456789@icloud.com>
2024-04-24 00:15:51 -0700

    update 16Fsmserial.v

--- 2.Circuits/2.2 Seq Logic/2.2.5 FSM/16Fsmserial.v
+++ 2.Circuits/2.2 Seq Logic/2.2.5 FSM/16Fsmserial.v
@@ -4,8 +4,7 @@ module top_module(
     input reset,    // Synchronous reset
     output done
 ); 
-    parameter WAIT = 0;
-    parameter START = 1
+    parameter IDLE = 0;
     parameter S0 = 1;
     parameter S1 = 2;
     parameter S2 = 3;
@@ -14,16 +13,15 @@ module top_module(
     parameter S5 = 6;
     parameter S6 = 7;
     parameter S7 = 8;
-    parameter STOP = 9
-    parameter ERROR = 10;
-    parameter DONE1 = 11;
-    parameter DONE2 = 13;
+    parameter STOP = 9;
+    parameter ERROR = 11;
+    parameter DONE = 12; 
 
     reg [3:0] state, next_state;
 
     always @(posedge clk) begin
         if(reset) begin
-            state <= WAIT;
+            state <= IDLE;
         end else begin
             state <= next_state;
         end
@@ -31,8 +29,7 @@ module top_module(
 
     always @(*) begin
         case(state)
-            WAIT: next_state = in?WAIT:START;
-            START: next_state = S0;
+            IDLE: next_state = in?IDLE:S0;
             S0: next_state = S1;
             S1: next_state = S2;
             S2: next_state = S3;
@@ -40,15 +37,12 @@ module top_module(
             S4: next_state = S5;
             S5: next_state = S6;
             S6: next_state = S7;
-            S7: next_state = S8;
-            S8: next_state = in?STOP:ERROR;
-            STOP: next_state = in?DONE1:DONE2;
-            DONE1: next_state = in?WAIT:START;
-            DONE2: next_state = S0;
+            S7: next_state = STOP;
+            STOP: next_state = in?DONE:ERROR;
+            DONE: next_state = in?IDLE:S0;
+            ERROR: next_state = in?IDLE:ERROR;
         endcase
     end
     assign done = (state == DONE);
-
-
 
 endmodule
