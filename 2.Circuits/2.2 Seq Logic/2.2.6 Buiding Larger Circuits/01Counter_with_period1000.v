60b1517cd1835e7e0b9f0194303de9c8930ca153
jiawey1227 <yujiawei123456789@icloud.com>
2024-05-19 22:21:25 -0700

    add counter1000.v

---
+++ 2.Circuits/2.2 Seq Logic/2.2.6 Buiding Larger Circuits/01Counter_with_period1000.v
@@ -0,0 +1,18 @@
+module top_module (
+    input clk,
+    input reset,
+    output [9:0] q);
+
+    always @(posedge clk) begin
+        if(reset) begin
+            q <= 10'd0;
+        end else begin
+            if(q < 10'd999) begin
+                q <= q + 10'd1;
+            end else begin
+                q <= 10'd0;
+            end
+        end
+    end
+
+endmodule
